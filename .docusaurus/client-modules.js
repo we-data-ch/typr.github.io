2c397d3769c8f401e4afff30d2367498950d4a69
Fabrice <fab.hatege15@gmail.com>
2026-09-06 01:15:04 +0200

    Brand new documentation!

--- .docusaurus/client-modules.js
+++
@@ -1,7 +0,0 @@
-export default [
-  require("/home/fabrice/temp3/typr.github.io/.docusaurus/docusaurus-plugin-css-cascade-layers/default/layers.css"),
-  require("/home/fabrice/temp3/typr.github.io/node_modules/infima/dist/css/default/default.css"),
-  require("/home/fabrice/temp3/typr.github.io/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/home/fabrice/temp3/typr.github.io/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/home/fabrice/temp3/typr.github.io/src/css/custom.css"),
-];
