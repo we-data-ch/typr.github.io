461e7cb50c87dba30bce602d24fbdecf6883a216
Fabrice <fab.hatege15@gmail.com>
2026-09-08 22:18:18 +0200

    added online markdown editor

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -46,7 +46,7 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/we-data-ch/typr.github.io/tree/main/',
+            'https://github.com/we-data-ch/typr.github.io/edit/main/',
         },
         blog: {
           beforeDefaultRehypePlugins: [rehypeShikiTypR],
@@ -58,7 +58,7 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/we-data-ch/typr.github.io/tree/main/',
+            'https://github.com/we-data-ch/typr.github.io/edit/main/',
           // Useful options to enforce blogging best practices
           onInlineTags: 'warn',
           onInlineAuthors: 'warn',
